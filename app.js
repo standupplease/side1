043916bd1fc07311341efe884fe80281c916bfbb
Your Name <zack.chen@gini.tw>
2021-12-23 19:00:13 +0800

    1

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/user', (req, res) => {
+    var user = JSON.parse(req.body)
+    res.send('user info is ' + JSON.stringify(user))
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
