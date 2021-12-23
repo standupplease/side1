282bcf0554e8eaa5b88adec1eac311086a48ee7a
Your Name <zack.chen@gini.tw>
2021-12-23 19:02:41 +0800

    1

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/user', (req, res) => {
     var user = JSON.parse(req.body)
     res.send('user info is ' + JSON.stringify(user))
-  })
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log('sever is up')
+    console.log(`Example app listening at http://localhost:${port}`)
 })
